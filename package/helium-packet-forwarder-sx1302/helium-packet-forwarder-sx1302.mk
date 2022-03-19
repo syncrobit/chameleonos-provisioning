e8134cbf6f2185dbc724a34c5464b69a2faa221d
Calin Crisan <ccrisan@gmail.com>
2022-03-19 20:31:48 +0200

    helium-packet-forwarder-sx1302: Sync with final OS

--- package/helium-packet-forwarder-sx1302/helium-packet-forwarder-sx1302.mk
+++ package/helium-packet-forwarder-sx1302/helium-packet-forwarder-sx1302.mk
@@ -4,17 +4,13 @@
 #
 ################################################################################
 
-HELIUM_PACKET_FORWARDER_SX1302_VERSION = 2021.02.26.0
+HELIUM_PACKET_FORWARDER_SX1302_VERSION = 2021.06.04.0
 HELIUM_PACKET_FORWARDER_SX1302_SITE = $(call github,helium,sx1302_hal,$(HELIUM_PACKET_FORWARDER_SX1302_VERSION))
 HELIUM_PACKET_FORWARDER_SX1302_LICENSE = Apache-2.0
 HELIUM_PACKET_FORWARDER_SX1302_LICENSE_FILES = LICENSE
             
 define HELIUM_PACKET_FORWARDER_SX1302_BUILD_CMDS
-    (cd $(@D); \
-            CROSS_COMPILE=aarch64-none-linux-gnu- \
-            $(TARGET_MAKE_ENV) \
-            make \
-    )
+    cd $(@D) && CROSS_COMPILE=$(TARGET_CROSS) $(TARGET_MAKE_ENV) make
 endef
 
 define HELIUM_PACKET_FORWARDER_SX1302_INSTALL_TARGET_CMDS
@@ -24,10 +20,7 @@ define HELIUM_PACKET_FORWARDER_SX1302_INSTALL_TARGET_CMDS
     cp $(@D)/packet_forwarder/lora_pkt_fwd $(TARGET_DIR)/opt/packet_forwarder/bin/lora_pkt_fwd_sx1302
     cp $(@D)/util_chip_id/chip_id $(TARGET_DIR)/opt/packet_forwarder/bin
     cp $(@D)/util_net_downlink/net_downlink $(TARGET_DIR)/opt/packet_forwarder/bin
-    for file in $(@D)/packet_forwarder/global_conf.json.sx1250.*; do \
-        fname=$$(basename "$${file}" | sed 's/sx1250/sx1302/'); \
-        cp "$${file}" $(TARGET_DIR)/opt/packet_forwarder/etc/$${fname}; \
-    done
+    cp package/helium-packet-forwarder-sx1302/global_conf.json.sx1302.* $(TARGET_DIR)/opt/packet_forwarder/etc/
     ln -sf /var/run/global_conf.json $(TARGET_DIR)/opt/packet_forwarder/bin/global_conf.json
 endef
 
