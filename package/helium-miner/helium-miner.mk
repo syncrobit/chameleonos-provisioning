1243a9a3ddefeee24fd08cc5faae576d01758500
Calin Crisan <ccrisan@gmail.com>
2022-08-08 15:05:10 +0300

    miner: Update package

--- package/helium-miner/helium-miner.mk
+++ package/helium-miner/helium-miner.mk
@@ -13,6 +13,8 @@ HELIUM_MINER_DEPENDENCIES = dbus gmp libsodium erlang host-rust-bin
 HELIUM_MINER_POST_EXTRACT_HOOKS += HELIUM_MINER_FETCH_PATCH_DEPS
 HELIUM_MINER_POST_EXTRACT_HOOKS += HELIUM_MINER_UPDATE_VERSION
 
+HELIUM_MINER_BUILD_AS = prod
+
 define HELIUM_MINER_FETCH_PATCH_DEPS
     (cd $(@D); $(TARGET_MAKE_ENV) ./rebar3 get-deps)
 
@@ -26,6 +28,13 @@ define HELIUM_MINER_UPDATE_VERSION
 endef
             
 define HELIUM_MINER_BUILD_CMDS
+    (cd $(@D); \
+            CARGO_HOME=$(HOST_DIR)/share/cargo \
+            CARGO_BUILD_TARGET=aarch64-unknown-linux-gnu \
+            RUSTFLAGS="-C target-feature=-crt-static" \
+            $(TARGET_MAKE_ENV) \
+            $(MAKE) external_svcs \
+    )
     (cd $(@D); ERTS_VERSION=$$(ls -d $(STAGING_DIR)/usr/lib/erlang/erts-* | head -n1 | xargs basename | grep -oE [0-9.]+); \
             CC="$(TARGET_CC)" \
             CXX="$(TARGET_CXX)" \
@@ -35,10 +44,11 @@ define HELIUM_MINER_BUILD_CMDS
             ERLANG_ROCKSDB_OPTS="-DWITH_BUNDLE_SNAPPY=ON -DWITH_BUNDLE_LZ4=ON" \
             ERL_COMPILER_OPTIONS="[deterministic]" \
             ERTS_INCLUDE_DIR="$(STAGING_DIR)/usr/lib/erlang/erts-$${ERTS_VERSION}/include" \
-            $(TARGET_MAKE_ENV) \
             CARGO_HOME=$(HOST_DIR)/share/cargo \
             CARGO_BUILD_TARGET=aarch64-unknown-linux-gnu \
-            ./rebar3 as prod tar -n miner \
+            RUSTFLAGS="-C target-feature=-crt-static" \
+            $(TARGET_MAKE_ENV) \
+            ./rebar3 as $(HELIUM_MINER_BUILD_AS) tar -n miner -v $(HELIUM_MINER_VERSION) \
     )
 endef
 
@@ -46,12 +56,15 @@ define HELIUM_MINER_INSTALL_TARGET_CMDS
     rm -rf $(TARGET_DIR)/opt/miner; \
     mkdir -p $(TARGET_DIR)/opt/miner; \
     cd $(TARGET_DIR)/opt/miner; \
-    tar xvf $(@D)/_build/prod/rel/*/*.tar.gz; \
+    tar xvf $(@D)/_build/$(HELIUM_MINER_BUILD_AS)/rel/miner/*.tar.gz; \
+    cp $(@D)/config/testnet-sys.config $(TARGET_DIR)/opt/miner/releases/*/; \
     mkdir -p update; \
-    wget https://github.com/helium/blockchain-api/raw/master/priv/prod/genesis -O update/genesis; \
+    wget https://snapshots.helium.wtf/genesis.mainnet -O update/genesis.mainnet; \
+    wget https://snapshots.helium.wtf/genesis.testnet -O update/genesis.testnet; \
     cp $(TARGET_DIR)/usr/lib/erlang/bin/no_dot_erlang.boot .
     
     rm -rf $(TARGET_DIR)/opt/miner/$${HOME}
+    rm -rf $(TARGET_DIR)/etc/helium_gateway
     cp $(@D)/config/com.helium.Miner.conf $(TARGET_DIR)/etc/dbus-1/system.d
 endef
 
@@ -60,5 +73,16 @@ endef
 
 define HELIUM_MINER_INSTALL_CMDS
 endef
+
+define HELIUM_MINER_TOOLCHAIN_ADJUST_GCC_CMDS
+    # On radxacm3 (default ARM toolchain), compiler is called aarch64-none-linux-gnu-gcc,
+    # but external/gateway-rs/longfi-sys expects aarch64-linux-gnu-gcc.
+    test -f $(HOST_DIR)/bin/aarch64-none-linux-gnu-gcc && \
+        ln -s aarch64-none-linux-gnu-gcc $(HOST_DIR)/bin/aarch64-linux-gnu-gcc || true
+    test -f $(HOST_DIR)/opt/ext-toolchain/bin/aarch64-none-linux-gnu-gcc && \
+        ln -sf aarch64-none-linux-gnu-gcc $(HOST_DIR)/opt/ext-toolchain/bin/aarch64-linux-gnu-gcc || true
+endef
+
+TOOLCHAIN_POST_INSTALL_TARGET_HOOKS += HELIUM_MINER_TOOLCHAIN_ADJUST_GCC_CMDS
 
 $(eval $(generic-package))
